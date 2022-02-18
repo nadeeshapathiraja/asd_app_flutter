ff0ded44ab13b4a4fc4c3104962c67c01f0fecfa
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-02-18 14:48:34 +0530

    Custom tile add

--- lib/components/custom_bottom_sheet.dart
+++ lib/components/custom_bottom_sheet.dart
@@ -14,39 +14,60 @@ class CustomBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Container(
+      height: 440,
       width: double.infinity,
       color: kwhite,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Container(
-              height: 100,
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.travel_explore,
-                    color: primaryColor,
-                    size: 30,
-                  ),
-                  SizedBox(width: 30),
-                  CustomText(
-                    text: "Browse Web",
-                    color: darkColor,
-                    fontsize: 25,
-                  )
-                ],
-              ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TileSection(),
+            TileSection(),
+            TileSection(),
+            TileSection(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TileSection extends StatelessWidget {
+  const TileSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Container(
+            height: 100,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.travel_explore,
+                  color: primaryColor,
+                  size: 30,
+                ),
+                SizedBox(width: 30),
+                CustomText(
+                  text: "Browse Web",
+                  color: darkColor,
+                  fontsize: 25,
+                )
+              ],
             ),
           ),
-          const Divider(
-            height: 20,
-            thickness: 1,
-            endIndent: 0,
-            color: primaryColor,
-          ),
-        ],
-      ),
+        ),
+        const Divider(
+          height: 10,
+          thickness: 1,
+          endIndent: 0,
+          color: primaryColor,
+        ),
+      ],
     );
   }
 }
--- lib/views/category_screens/add_category.dart
+++ lib/views/category_screens/add_category.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
+import 'package:game_app/components/custom_bottom_sheet.dart';
 import 'package:game_app/components/custom_button.dart';
 import 'package:game_app/components/custom_input.dart';
 import 'package:game_app/components/custom_text.dart';
@@ -101,7 +102,8 @@ class _AddCategoryState extends State<AddCategory> {
                           onPressed: () {
                             showModalBottomSheet(
                               context: context,
-                              builder: (builder) => bottomSheet(),
+                              builder: (builder) =>
+                                  CustomBottomSheet(context: context),
                             );
                           },
                           icon: Icon(
@@ -139,45 +141,6 @@ class _AddCategoryState extends State<AddCategory> {
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  Widget bottomSheet() {
-    final size = MediaQuery.of(context).size;
-    return Container(
-      width: double.infinity,
-      color: kwhite,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Container(
-              height: 100,
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.travel_explore,
-                    color: primaryColor,
-                    size: 30,
-                  ),
-                  SizedBox(width: 30),
-                  CustomText(
-                    text: "Browse Web",
-                    color: darkColor,
-                    fontsize: 25,
-                  )
-                ],
-              ),
-            ),
-          ),
-          const Divider(
-            height: 20,
-            thickness: 1,
-            endIndent: 0,
-            color: primaryColor,
-          ),
-        ],
       ),
     );
   }
