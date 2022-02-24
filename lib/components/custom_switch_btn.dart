0d8aa9e5f41d8bf763ffbb5f25d9ccbb4bd4aa22
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-02-24 22:05:31 +0530

    add switch to home page

--- lib/components/custom_switch_btn.dart
+++ lib/components/custom_switch_btn.dart
@@ -3,11 +3,19 @@ import 'package:flutter_switch/flutter_switch.dart';
 import 'package:game_app/components/custom_text.dart';
 import 'package:game_app/providers/mode_change_provider.dart';
 import 'package:game_app/utils/app_colors.dart';
+import 'package:game_app/utils/util_functions.dart';
+import 'package:game_app/views/home_screen/home_screen.dart';
 import 'package:provider/provider.dart';
 
-class Swicthbtn extends StatelessWidget {
-  const Swicthbtn({Key? key}) : super(key: key);
+class CustomSwitchBtn extends StatefulWidget {
+  const CustomSwitchBtn({Key? key}) : super(key: key);
 
+  @override
+  State<CustomSwitchBtn> createState() => _CustomSwitchBtnState();
+}
+
+class _CustomSwitchBtnState extends State<CustomSwitchBtn> {
+  bool mode = false;
   @override
   Widget build(BuildContext context) {
     return Consumer<ModeProvider>(
@@ -26,11 +34,12 @@ class Swicthbtn extends StatelessWidget {
               padding: 5.0,
               activeColor: activeColor,
               onToggle: (val) {
-                value.changeMode(val);
-                // setState(() {
-                //   mode = val;
-                //   value.changeMode(val);
-                // });
+                // value.changeMode(val);
+                setState(() {
+                  mode = val;
+                  value.changeMode(val);
+                });
+                UtilFunction.navigateTo(context, HomeScreen());
               },
             ),
           ],
--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:game_app/components/custom_button.dart';
+import 'package:game_app/components/custom_switch_btn.dart';
 import 'package:game_app/components/custom_text.dart';
 import 'package:game_app/providers/mode_change_provider.dart';
 import 'package:game_app/providers/user_provider.dart';
@@ -22,7 +23,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool mode = false;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -44,97 +44,79 @@ class _HomeScreenState extends State<HomeScreen> {
               top: 10.0,
             ),
             //Add consumer for mode change
-            child: Consumer<ModeProvider>(
-              builder: (context, value, child) {
-                return SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          CustomText(text: "PARENT MODE"),
-                          SizedBox(width: 10),
-                          FlutterSwitch(
-                            width: 50.0,
-                            height: 25.0,
-                            toggleSize: 18.0,
-                            value: value.getMode,
-                            borderRadius: 30.0,
-                            padding: 5.0,
-                            activeColor: activeColor,
-                            onToggle: (val) {
-                              value.changeMode(val);
-                              // setState(() {
-                              //   mode = val;
-                              //   value.changeMode(val);
-                              // });
-                            },
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      const CustomText(
-                        text: "Welcome",
-                        fontsize: 40,
-                        color: darkColor,
-                      ),
-                      const SizedBox(height: 30),
-                      const CustomText(
-                        text: "What would you like\nto do today",
-                        fontsize: 18,
-                        color: darkColor,
-                      ),
-                      const SizedBox(height: 30),
-                      Column(
-                        children: [
-                          ActivityCard(
-                            size: size,
-                            onTap: () {
-                              if (mode == true) {
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Parent mode changer
+                CustomSwitchBtn(),
+                const SizedBox(height: 20),
+                const CustomText(
+                  text: "Welcome",
+                  fontsize: 40,
+                  color: darkColor,
+                ),
+                const SizedBox(height: 30),
+                const CustomText(
+                  text: "What would you like\nto do today",
+                  fontsize: 18,
+                  color: darkColor,
+                ),
+                const SizedBox(height: 30),
+                Column(
+                  children: [
+                    Consumer<ModeProvider>(
+                      builder: (context, value, child) {
+                        return Column(
+                          children: [
+                            ActivityCard(
+                              size: size,
+                              onTap: () {
+                                if (value.getMode == true) {
+                                  UtilFunction.navigateTo(
+                                      context, CatergoryList());
+                                } else {
+                                  UtilFunction.navigateTo(
+                                      context, DisplayCatergory());
+                                }
+                              },
+                              title: "Vocabulary",
+                            ),
+                            SizedBox(height: 30),
+                            ActivityCard(
+                              size: size,
+                              onTap: () {
                                 UtilFunction.navigateTo(
-                                    context, CatergoryList());
-                              } else {
-                                UtilFunction.navigateTo(
-                                    context, DisplayCatergory());
-                              }
-                            },
-                            title: "Vocabulary",
-                          ),
-                          SizedBox(height: 30),
-                          ActivityCard(
-                            size: size,
-                            onTap: () {
-                              UtilFunction.navigateTo(context, DisplayVideos());
-                            },
-                            title: "Video Activity",
-                          ),
-                          SizedBox(height: 30),
-                          ActivityCard(
-                            size: size,
-                            onTap: () {
-                              UtilFunction.navigateTo(context, DisplayGame());
-                            },
-                            title: "Play time",
-                          ),
-                          SizedBox(height: 30),
-                          value.getMode
-                              ? CustomButton(
-                                  size: size,
-                                  textValue: "SignOut",
-                                  onTap: () {
-                                    Provider.of<UserProvider>(context,
-                                            listen: false)
-                                        .logout(context);
-                                  },
-                                )
-                              : CustomText(text: ""),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              },
+                                    context, DisplayVideos());
+                              },
+                              title: "Video Activity",
+                            ),
+                            SizedBox(height: 30),
+                            ActivityCard(
+                              size: size,
+                              onTap: () {
+                                UtilFunction.navigateTo(context, DisplayGame());
+                              },
+                              title: "Play time",
+                            ),
+                            SizedBox(height: 30),
+                            value.getMode
+                                ? CustomButton(
+                                    size: size,
+                                    textValue: "SignOut",
+                                    onTap: () {
+                                      Provider.of<UserProvider>(context,
+                                              listen: false)
+                                          .logout(context);
+                                    },
+                                  )
+                                : CustomText(text: ""),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
