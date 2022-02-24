92668374a9dd95c116999c173e6d6bad6819bc78
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-02-25 02:16:24 +0530

    Add usermodel

---
+++ lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  final String uid;
+  final String email;
+  final String phone;
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.phone,
+  });
+}
