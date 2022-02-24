5d6ab785d791cf9338efd5b29932516d3f844a13
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-02-25 01:57:55 +0530

    clear code

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:game_app/providers/auth/register_provider.dart';
 import 'package:game_app/providers/category_provider.dart';
 import 'package:game_app/providers/mode_change_provider.dart';
 import 'package:game_app/providers/user_provider.dart';
-import 'package:game_app/views/category_screens/add_category.dart';
+import 'package:game_app/views/splash_screen/splash.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
-      home: const AddCategory(),
+      home: const SplashScreen(),
       //Responsive design
       builder: (context, widget) => ResponsiveWrapper.builder(
         widget,
