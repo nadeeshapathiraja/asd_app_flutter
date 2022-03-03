d74820d978bb9ed2f385cce4f62e4497165a81f3
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-03-03 23:59:44 +0530

    Video screen

--- lib/views/video_screens/display_videos.dart
+++ lib/views/video_screens/display_videos.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:game_app/components/custom_backbutton.dart';
+import 'package:game_app/components/custom_switch_btn.dart';
+import 'package:game_app/utils/app_colors.dart';
+import 'package:game_app/utils/constants.dart';
 
 class DisplayVideos extends StatefulWidget {
   const DisplayVideos({Key? key}) : super(key: key);
@@ -10,12 +14,57 @@ class DisplayVideos extends StatefulWidget {
 class _DisplayVideosState extends State<DisplayVideos> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [],
+        body: SingleChildScrollView(
+      child: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Constants.imageAssets("bg.jpg")),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 10.0,
+              right: 10.0,
+              top: 10.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CustomSwitchBtn(),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    //Custom Back button
+                    CustomBackButton(),
+                    SizedBox(
+                      width: size.width * 0.25,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Container(
+                      width: size.width,
+                      // height: size.height * 0.8,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: kwhite,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
-    );
+    ));
   }
 }
