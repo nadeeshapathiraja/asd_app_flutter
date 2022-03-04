5af4fa7575cac79eb5351e05a5cb8e12b9766d1b
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-03-04 21:38:55 +0530

    Clear video screen code

--- lib/providers/video_screen_provider.dart
+++ lib/providers/video_screen_provider.dart
@@ -1,25 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class VideoScreenProvider extends ChangeNotifier {
-  TextEditingController _addItem = TextEditingController();
-  late YoutubePlayerController _youtubePlayerController;
-  late DocumentReference linkRef;
-  List<String> videoId = [];
-  bool shoItem = false;
-
-  //get Video url from input field
-  TextEditingController get getUrl => _addItem;
-
-  List<String> get getVideoId => videoId;
-
-  addItemFunction() async {
-    await linkRef.set(
-      {
-        _addItem.text.toString(): _addItem.text.toString(),
-      },
-      SetOptions(merge: true),
-    );
-  }
-}
+class VideoScreenProvider extends ChangeNotifier {}
--- lib/views/video_screens/display_videos.dart
+++ lib/views/video_screens/display_videos.dart
@@ -1,14 +1,7 @@
-// ignore_for_file: unnecessary_null_comparison
-
 import 'package:flutter/material.dart';
 import 'package:game_app/components/custom_backbutton.dart';
-import 'package:game_app/components/custom_input.dart';
 import 'package:game_app/components/custom_switch_btn.dart';
-import 'package:game_app/providers/video_screen_provider.dart';
-import 'package:game_app/utils/app_colors.dart';
 import 'package:game_app/utils/constants.dart';
-import 'package:provider/provider.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class DisplayVideos extends StatefulWidget {
   const DisplayVideos({Key? key}) : super(key: key);
@@ -53,63 +46,6 @@ class _DisplayVideosState extends State<DisplayVideos> {
                   ],
                 ),
                 const SizedBox(height: 10),
-                Expanded(
-                  child: Container(
-                    child: Consumer<VideoScreenProvider>(
-                      builder: (context, value, child) {
-                        return Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Column(
-                            children: [
-                              CustomInput(
-                                controller: value.getUrl,
-                                lableText: 'Input Url',
-                                hintText: "Youtube URL",
-                                iconBtn: IconButton(
-                                  icon: Icon(Icons.add),
-                                  onPressed: () {
-                                    if (value.getUrl.text != null) {
-                                      value.addItemFunction();
-                                    }
-                                  },
-                                  iconSize: 30,
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              Container(
-                                child: Expanded(
-                                  child: ListView.builder(
-                                    itemCount: value.getVideoId.length,
-                                    itemBuilder: (context, index) {
-                                      return Container(
-                                        margin: EdgeInsets.all(8.0),
-                                        child: YoutubePlayer(
-                                          controller: YoutubePlayerController(
-                                            initialVideoId:
-                                                value.getVideoId[index],
-                                            flags: YoutubePlayerFlags(
-                                              autoPlay: false,
-                                            ),
-                                          ),
-                                          showVideoProgressIndicator: true,
-                                          progressIndicatorColor: kBlue,
-                                          progressColors: ProgressBarColors(
-                                            playedColor: kBlue,
-                                            handleColor: kGrey,
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                )
               ],
             ),
           ),
