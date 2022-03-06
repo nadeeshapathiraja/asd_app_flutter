ba450daab9761132650d79756cfc44fa87a2125b
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-03-06 23:07:56 +0530

    Video screen provider add and change

--- lib/views/video_screens/display_videos.dart
+++ lib/views/video_screens/display_videos.dart
@@ -4,9 +4,6 @@ import 'package:game_app/components/custom_switch_btn.dart';
 import 'package:game_app/providers/video_screen_provider.dart';
 import 'package:game_app/utils/constants.dart';
 import 'package:provider/provider.dart';
-import 'package:video_player/video_player.dart';
-import 'package:videos_player/model/video.model.dart';
-import 'package:videos_player/videos_player.dart';
 
 class DisplayVideos extends StatefulWidget {
   const DisplayVideos({Key? key}) : super(key: key);
@@ -16,23 +13,6 @@ class DisplayVideos extends StatefulWidget {
 }
 
 class _DisplayVideosState extends State<DisplayVideos> {
-  late VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.asset(Constants.videoPath)
-      ..addListener(() => setState(() {}))
-      ..setLooping(true)
-      ..initialize().then((_) => _controller.play());
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -70,21 +50,16 @@ class _DisplayVideosState extends State<DisplayVideos> {
                 const SizedBox(height: 10),
                 Consumer<VideoScreenProvider>(
                   builder: (context, value, child) {
-                    return ListView.builder(
-                        itemCount: value.getVideoList.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return VideosPlayer(
-                            networkVideos: [
-                              NetworkVideo(
-                                id: value.getVideoList[index]['id'],
-                                name: value.getVideoList[index]['name'],
-                                videoUrl: value.getVideoList[index]['videoUrl'],
-                                thumbnailUrl: value.getVideoList[index]
-                                    ['thumbnailUrl'],
-                              )
-                            ],
-                          );
-                        });
+                    return Expanded(
+                      child: Container(
+                        child: ListView.builder(
+                            itemCount: value.getVideoList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Text(
+                                  value.getVideoList[index]['videoUrl']);
+                            }),
+                      ),
+                    );
                   },
                 )
               ],
