92908fd73753752c91d1f7f89414a9c8aebed4d9
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-03-07 12:45:20 +0530

    Test video player added

--- lib/views/video_screens/display_videos.dart
+++ lib/views/video_screens/display_videos.dart
@@ -4,6 +4,7 @@ import 'package:game_app/components/custom_switch_btn.dart';
 import 'package:game_app/providers/video_screen_provider.dart';
 import 'package:game_app/utils/constants.dart';
 import 'package:provider/provider.dart';
+import 'package:video_player/video_player.dart';
 
 class DisplayVideos extends StatefulWidget {
   const DisplayVideos({Key? key}) : super(key: key);
@@ -13,6 +14,25 @@ class DisplayVideos extends StatefulWidget {
 }
 
 class _DisplayVideosState extends State<DisplayVideos> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -48,23 +68,45 @@ class _DisplayVideosState extends State<DisplayVideos> {
                   ],
                 ),
                 const SizedBox(height: 10),
-                Consumer<VideoScreenProvider>(
-                  builder: (context, value, child) {
-                    return Expanded(
-                      child: Container(
-                        child: ListView.builder(
-                            itemCount: value.getVideoList.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return Text(
-                                  value.getVideoList[index]['videoUrl']);
-                            }),
-                      ),
-                    );
-                  },
-                )
+                // Consumer<VideoScreenProvider>(
+                //   builder: (context, value, child) {
+                //     return Expanded(
+                //       child: Container(
+                //         child: ListView.builder(
+                //             itemCount: value.getVideoList.length,
+                //             itemBuilder: (BuildContext context, int index) {
+                //               return Text(
+                //                   value.getVideoList[index]['videoUrl']);
+                //             }),
+                //       ),
+                //     );
+                //   },
+                // ),
+
+                //Video Player
+                Center(
+                  child: _controller.value.isInitialized
+                      ? AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller),
+                        )
+                      : Container(),
+                ),
               ],
             ),
           ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
