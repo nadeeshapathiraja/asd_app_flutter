227831c4e4633662a24adcf9a90114b1a23b25dd
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-03-11 14:42:57 +0530

    Game one background done

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,13 +4,20 @@ class Constants {
   static const iconPath = 'assets/icons/';
   static const audioPath = 'assets/audios/';
   static const videoPath = 'assets/videos/';
+  static const gamePath = 'assets/games/';
 
   //Image assets functions
   static String imageAssets(img) => '$imagePath$img';
+
   //icon assets Function
   static String iconAssets(icon) => '$iconPath$icon';
+
   //audio assets Function
   static String audioAssets(audio) => '$audioPath$audio';
+
   //video assets Function
   static String videoAssets(video) => '$videoPath$video';
+
+  //game assets functions
+  static String gameAssets(game) => '$gamePath$game';
 }
--- lib/views/game_screens/game_one/game_one.dart
+++ lib/views/game_screens/game_one/game_one.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:game_app/components/custom_backbutton.dart';
+import 'package:game_app/components/custom_switch_btn.dart';
+import 'package:game_app/components/custom_text.dart';
+import 'package:game_app/utils/app_colors.dart';
+import 'package:game_app/utils/constants.dart';
 
 class GameOne extends StatefulWidget {
   const GameOne({Key? key}) : super(key: key);
@@ -10,6 +16,61 @@ class GameOne extends StatefulWidget {
 class _GameOneState extends State<GameOne> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Constants.imageAssets("bg.jpg")),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+              padding: const EdgeInsets.only(
+                left: 10.0,
+                right: 10.0,
+                top: 10.0,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CustomSwitchBtn(),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        //Custom Back button
+                        CustomBackButton(),
+                        SizedBox(
+                          width: size.width * 0.3,
+                        ),
+                        CustomText(
+                          text: "Let's Play",
+                          fontWeight: FontWeight.w700,
+                          fontsize: 40,
+                          color: darkColor,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Stack(
+                      children: [
+                        Image.asset(
+                          Constants.gameAssets('gamebg.png'),
+                          width: size.width * 0.99,
+                          height: size.height * 0.65,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )),
+        ),
+      ),
+    ));
   }
 }
