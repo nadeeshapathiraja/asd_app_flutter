bfe486523695a99bc82e16873cd89b118254713d
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-03-11 14:52:15 +0530

    Game one background don

--- lib/views/game_screens/game_two/game_two.dart
+++ lib/views/game_screens/game_two/game_two.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:game_app/components/custom_backbutton.dart';
 import 'package:game_app/components/custom_switch_btn.dart';
@@ -6,14 +5,14 @@ import 'package:game_app/components/custom_text.dart';
 import 'package:game_app/utils/app_colors.dart';
 import 'package:game_app/utils/constants.dart';
 
-class GameOne extends StatefulWidget {
-  const GameOne({Key? key}) : super(key: key);
+class GameTwo extends StatefulWidget {
+  const GameTwo({Key? key}) : super(key: key);
 
   @override
-  State<GameOne> createState() => _GameOneState();
+  State<GameTwo> createState() => _GameTwoState();
 }
 
-class _GameOneState extends State<GameOne> {
+class _GameTwoState extends State<GameTwo> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -64,14 +63,6 @@ class _GameOneState extends State<GameOne> {
                           width: size.width * 0.99,
                           height: size.height * 0.65,
                         ),
-                        Positioned(
-                          bottom: 20,
-                          left: 20,
-                          child: Image.asset(
-                            Constants.gameAssets('bird.png'),
-                            width: 200,
-                          ),
-                        )
                       ],
                     )
                   ],
