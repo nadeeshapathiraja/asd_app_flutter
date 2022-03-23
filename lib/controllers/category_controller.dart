48a9e9e45bd92492e39e193c5b0a04e1edb75d4d
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-03-23 22:23:26 +0530

    hardcode uid added

--- lib/controllers/category_controller.dart
+++ lib/controllers/category_controller.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:logger/logger.dart';
+import 'package:path/path.dart';
+
 class CategoryController {
   //Firestore inctanec create
   FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -20,10 +23,29 @@ class CategoryController {
 
     await category.doc(docId).set({
       'id': docId,
-      'uid': 1,
+      'uid': uid,
       'name': name,
       'img': "https://www.imgacademy.com/sites/default/files/home-bs.jpg",
       'voice': "https://www.imgacademy.com/sites/default/files/home-bs.jpg",
     });
+
+    // UploadTask? task = uploadImg(img);
+    // final snapshot = await task!.whenComplete(() {});
+    // final downloadUrd = await snapshot.ref.getDownloadURL();
+
+    // Logger().i(downloadUrd);
   }
+
+  // //Upload file to db
+  // UploadTask? uploadImg(File file) {
+  //   try {
+  //     final fileName = basename(file.path);
+  //     final destination = 'categoryImage/$fileName';
+  //     final ref = FirebaseStorage.instance.ref(destination);
+  //     return ref.putFile(file);
+  //   } catch (e) {
+  //     Logger().e(e);
+  //     return null;
+  //   }
+  // }
 }
