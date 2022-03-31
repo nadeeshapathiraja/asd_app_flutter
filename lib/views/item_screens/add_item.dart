5283d02693d0930f8caac77e79919ccc4ea18789
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-04-01 02:22:45 +0530

    Add item provider pgae

--- lib/views/item_screens/add_item.dart
+++ lib/views/item_screens/add_item.dart
@@ -5,6 +5,7 @@ import 'package:game_app/components/custom_input.dart';
 import 'package:game_app/components/custom_main_navbar.dart';
 import 'package:game_app/components/custom_text.dart';
 import 'package:game_app/providers/category_provider.dart';
+import 'package:game_app/providers/itrm_provider.dart';
 import 'package:game_app/utils/app_colors.dart';
 import 'package:game_app/utils/constants.dart';
 import 'package:game_app/utils/util_functions.dart';
@@ -35,93 +36,97 @@ class _AddItemState extends State<AddItem> {
               fit: BoxFit.cover,
             ),
           ),
-          child: SafeArea(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  right: 20.0,
-                  top: 10.0,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    //main Navbar
-                    CustomMainNavBar(),
-                    const SizedBox(height: 30),
-                    const CustomText(
-                      text: "Add New Fruit",
-                      fontsize: 40,
-                      color: darkColor,
+          child: Consumer<ItemProvider>(
+            builder: (context, value, child) {
+              return SafeArea(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20.0,
+                      right: 20.0,
+                      top: 10.0,
                     ),
-                    const SizedBox(height: 50),
-                    CustomInput(
-                      controller: _email,
-                      lableText: "Name",
-                    ),
-                    const SizedBox(height: 30),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        InkWell(
-                          onTap: () {
-                            showModalBottomSheet(
-                              context: context,
-                              builder: (builder) =>
-                                  CustomBottomSheet(context: context),
-                            );
-                          },
-                          child: Stack(
-                            children: [
-                              Consumer<CategoryProvider>(
-                                builder: (context, value, child) {
-                                  return Image.asset(
-                                    Constants.imageAssets("logo.png"),
-                                    width: 150,
-                                    height: 150,
-                                  );
-                                },
-                                // value.imageFile == null ?
-                                // FileImage(File(value.imageFile.path)
+                        //main Navbar
+                        CustomMainNavBar(),
+                        const SizedBox(height: 30),
+                        const CustomText(
+                          text: "Add New Fruit",
+                          fontsize: 40,
+                          color: darkColor,
+                        ),
+                        const SizedBox(height: 50),
+                        CustomInput(
+                          controller: _email,
+                          lableText: "Name",
+                        ),
+                        const SizedBox(height: 30),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  builder: (builder) =>
+                                      CustomBottomSheet(context: context),
+                                );
+                              },
+                              child: Stack(
+                                children: [
+                                  Consumer<CategoryProvider>(
+                                    builder: (context, value, child) {
+                                      return Image.asset(
+                                        Constants.imageAssets("logo.png"),
+                                        width: 150,
+                                        height: 150,
+                                      );
+                                    },
+                                    // value.imageFile == null ?
+                                    // FileImage(File(value.imageFile.path)
+                                  ),
+                                  Positioned(
+                                    bottom: 20,
+                                    right: 20,
+                                    child: Icon(
+                                      Icons.camera_alt,
+                                      size: 30,
+                                    ),
+                                  ),
+                                ],
                               ),
-                              Positioned(
-                                bottom: 20,
-                                right: 20,
-                                child: Icon(
-                                  Icons.camera_alt,
-                                  size: 30,
-                                ),
-                              ),
-                            ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 30),
+                        CustomInput(
+                          controller: _email,
+                          lableText: "How is it pronounced?",
+                          iconBtn: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.mic,
+                              size: 30,
+                              color: primaryColor,
+                            ),
                           ),
+                        ),
+                        const SizedBox(height: 50),
+                        CustomButton(
+                          size: size,
+                          onTap: () {
+                            UtilFunction.navigateTo(context, ItemList());
+                          },
+                          textValue: "Done",
                         ),
                       ],
                     ),
-                    const SizedBox(height: 30),
-                    CustomInput(
-                      controller: _email,
-                      lableText: "How is it pronounced?",
-                      iconBtn: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.mic,
-                          size: 30,
-                          color: primaryColor,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    CustomButton(
-                      size: size,
-                      onTap: () {
-                        UtilFunction.navigateTo(context, ItemList());
-                      },
-                      textValue: "Done",
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
+              );
+            },
           ),
         ),
       ),
